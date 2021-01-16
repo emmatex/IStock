73f1153f1f722ff036ba3ee672f0b332016dfe33
emmatex <emmatex4yall@hotmail.com>
2021-01-16 17:41:47 +0100

    product

--- Core/Entities/Product.cs
+++ Core/Entities/Product.cs
@@ -3,6 +3,7 @@
     public class Product : BaseEntity
     {
         public string Name { get; set; }
+        public string Code { get; set; }
         public string Description { get; set; }
         public string MeasurementName { get; set; }
         public decimal Price { get; set; }
