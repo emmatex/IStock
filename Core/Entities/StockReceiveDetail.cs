12f50852d479752ddc74256c903e5acaa9ac331d
emmatex <emmatex4yall@hotmail.com>
2021-01-16 18:49:23 +0100

    Igeneric repo

--- Core/Entities/StockReceiveDetail.cs
+++ Core/Entities/StockReceiveDetail.cs
@@ -4,6 +4,11 @@
     {
         public int BillQuantity { get; set; }
         public int ReceivedQuantity { get; set; }
-
+        public StockReceive StockReceive { get; set; }
+        public string StockReceiveId { get; set; }
+        public Product Product { get; set; }
+        public string ProductId { get; set; }
+        public StockState StockState { get; set; }
+        public string StockStateId { get; set; }
     }
 }
