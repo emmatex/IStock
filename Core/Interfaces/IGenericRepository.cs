b53fe5957d752b78ba6bcd21667f03c825bd4d89
emmatex <emmatex4yall@hotmail.com>
2021-01-16 18:52:05 +0100

    Ispecification

--- Core/Interfaces/IGenericRepository.cs
+++ Core/Interfaces/IGenericRepository.cs
@@ -1,11 +1,26 @@
 ﻿using Core.Entities;
+using Core.Specifications;
 using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
 
 namespace Core.Interfaces
 {
     public interface IGenericRepository<T> where T : BaseEntity
     {
+        Task<IReadOnlyList<T>> ListAllAsync();
+        Task<T> GetEntityWithSpec(ISpecification<T> spec);
+        Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec);
+        Task<IEnumerable<T>> GetWhere(Expression<Func<T, bool>> predicate);
+        Task<T> GetSingle(Expression<Func<T, bool>> predicate);
+        Task<int> CountAsync(ISpecification<T> spec);
+        bool IsExist(Func<T, bool> filter);
+        Task<bool> SaveChangesAsync();
+        Task AddRange(IEnumerable<T> entities);
+        Task<T> GetSingle();
+        Task Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
     }
 }
