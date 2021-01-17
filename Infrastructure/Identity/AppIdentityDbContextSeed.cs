6597c45f8b6077b4fb3b580deb8a57a8bcab7a49
emmatex <emmatex4yall@hotmail.com>
2021-01-17 14:19:58 +0100

    app identity seeding

---
+++ Infrastructure/Identity/AppIdentityDbContextSeed.cs
@@ -0,0 +1,34 @@
+﻿using Core.Entities.Identity;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Identity
+{
+    public class AppIdentityDbContextSeed
+    {
+        public static async Task SeedUsersAsync(UserManager<AppUser> userManager)
+        {
+            try
+            {
+                if (!userManager.Users.Any())
+                {
+                    var user = new AppUser
+                    {
+                        FullName = "System Admin",
+                        Email = "admin@istock.com",
+                        UserName = "admin",
+                        PhoneNumber = "07065153797",
+                    };
+                    await userManager.CreateAsync(user, "Pa$$w0rd");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+    }
+}
