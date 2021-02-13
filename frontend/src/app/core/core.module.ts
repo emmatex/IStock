015253e193e21b6e40a051f038da6e6070e8ed77
emmatex <emmatex4yall@hotmail.com>
2021-02-14 00:11:27 +0100

    core

--- frontend/src/app/core/core.module.ts
+++ frontend/src/app/core/core.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { SectionHeaderComponent } from './section-header/section-header.component';
 
 
 
 @NgModule({
-  declarations: [NotFoundComponent],
+  declarations: [NotFoundComponent, SectionHeaderComponent],
   imports: [
     CommonModule
   ]
