68901f98eaf2614b53c010f86a44503ef04ec093
emmatex <emmatex4yall@hotmail.com>
2021-02-14 00:08:21 +0100

    shared

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PagerComponent } from './components/pager/pager.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [PagerComponent],
   imports: [
     CommonModule
   ]
